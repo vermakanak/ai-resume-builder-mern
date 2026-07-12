fa29815b2ed70fb04c4a10b9a28463591a408492
kanak verma <kanakv2006@gamil.com>
2026-07-12 14:15:10 +0530

    updated backend

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,25 +1,17 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try{
-        mongoose.connection.on("connected",()=>{
-            console.log("Database connected successfully")
-        })
-        let mongodbURI = process.env.MONGODB_URI;
-        const projectName = 'resume-builder';
+    const mongodbURI = process.env.MONGODB_URI;
 
-        if(!mongodbURI){
-            throw new Error("MONGODB_URI environment variable not set")
-        }
-
-        if(mongodbURI.endsWith('/')){
-            mongodbURI = mongodbURI.slice(0,-1)
+    if (!mongodbURI) {
+        throw new Error("MONGODB_URI environment variable is not set");
     }
-    await mongoose.connect(`${mongodbURI}/${projectName}`)
-}
-    catch(error){
-        console.error("Error connecting to MongoDB:",error)
-}
 
-}
+    await mongoose.connect(mongodbURI, {
+        dbName: "resume-builder",
+    });
+
+    console.log("Database connected successfully");
+};
+
 export default connectDB;
